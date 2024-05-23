1dc98b89dea68796f393cd6309471077a47a7311
shao-chelin <a4061476@gmail.com>
2024-05-24 01:30:30 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/Standard_library_string_class_test_program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Standard_library_string_class_test_program.dir/main.cpp.o"
+  "CMakeFiles/Standard_library_string_class_test_program.dir/main.cpp.o.d"
+  "Standard_library_string_class_test_program"
+  "Standard_library_string_class_test_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Standard_library_string_class_test_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Standard_library_string_class_test_program.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Standard_library_string_class_test_program.
+# This may be replaced when dependencies are built.
